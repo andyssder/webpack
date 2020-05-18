60625a392d790ce24f02a31b33951a7bfaabb01a
andyssder <yuanhanghsu@gmail.com>
2020-05-18 17:31:25 +0800

    webpack-production

---
+++ webpack-production/README.md
@@ -0,0 +1,2 @@
+# webpack
+[webpack生产环境](https://webpack.docschina.org/guides/production/)
---
+++ webpack-production/src/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+function component() {
+    let element = document.createElement('div');
+    
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    return element;
+  }
+
+document.body.appendChild(component());
